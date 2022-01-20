d6d3904159778e4296003813737583f54afb8bdc
thiago lopes de mello <timabuntu@gmail.com>
2022-01-20 11:00:52 -0300

    feat: lines created

--- components/Square/index.jsx
+++ components/Square/index.jsx
@@ -1,10 +1,10 @@
 import styles from "../../styles/Square.module.css";
 
-const Square = (props) => {
+const Square = ({ dark }) => {
   return (
     <div
       style={{
-        backgroundColor: props.dark ? "#111" : "#f2f2f2",
+        backgroundColor: dark ? "#111" : "#f2f2f2",
       }}
       className={styles.square}
     ></div>
